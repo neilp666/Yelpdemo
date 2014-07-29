eb8910bfe8bc7c25362b2f4da91e5129facca3d6
neil patel <neil@myindianweddingapp.com>
2014-07-29 22:20:01 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+== README
+
+Yelp Demo 2014
+
+Things you may want to cover:
+
+* Ruby version 2.0
+
+* Rails 4.0.4
+
+* Bootstrap 3.0
+
+* Carrierwave
+
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yelpdemo::Application.config.session_store :cookie_store, key: '_yelpdemo_session'

acfe4bbf53d659041e702a254a6cf24a37eb105f
neil patel <neil@myindianweddingapp.com>
2014-08-03 12:15:35 +0100

    Display Review on Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,6 +11,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+    @reviews = Review.where(restaurant_id: @restaurant.id)
   end
 
   # GET /restaurants/new
